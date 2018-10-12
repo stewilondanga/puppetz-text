1a8cec1b88d51e363378adae712b106be6aa3736
Stewart ilondanga <stilondanga@gmail.com>
2018-10-12 12:58:12 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-/*var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+var navigate = (function() {
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
 
-"use strict";
+/*"use strict";
 {
 	const txt = 'EXPANDED';
 	let viscosity = 0.005;
